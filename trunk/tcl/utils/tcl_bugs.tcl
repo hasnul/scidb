2aedd18530a77553da16eded31179fc1674b720f
gcramer <gcramer@3f837a8a-01ec-4976-b98b-b3b4b9fec2bf>
2012-10-13 13:44:48 +0000

    fix for a bug in [namespace code ...] handling of Tcl 8.6

--- trunk/tcl/utils/tcl_bugs.tcl
+++ trunk/tcl/utils/tcl_bugs.tcl
@@ -31,4 +31,32 @@ proc trace {cmd type args} {
 	uplevel [list __trace__orig $cmd $type {*}$args]
 }
 
+
+if {[info tclversion] >= "8.6"} { ;#####################################
+
+# Work around for a severe Tcl 8.6 bug with [namespace code ...] handling.
+#
+# The response of the Tcl team to this bug item
+# http://sourceforge.net/tracker/index.php?func=detail&aid=2945212&group_id=10894&atid=110894
+# is really nonsense. Fact is that Tcl 8.5 scripts do not work anymore, and there is no known
+# substitute for [namespace code ...].
+
+rename unknown __unknown__orig
+
+proc unknown {args} {
+	set cmd [lindex $args 0]
+	if {[regexp "^:*namespace\[ \t\n\]+inscope" $cmd] && [llength $cmd] == 4} {
+		lassign $cmd _ _ ns cmd
+		set args [lrange $args 1 end]
+		catch { namespace eval $ns [list $cmd {*}$args] } result opts
+		dict unset opts -errorinfo
+		dict incr opts -level
+		return -options $opts $result
+	}
+
+	return [__unknown__orig {*}$args]
+}
+
+} ;# [info tclversion] >= "8.6"
+
 # vi:set ts=3 sw=3:
