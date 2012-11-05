a6a2b436ed204525f84829fbdbb070ca20ab6cb2
gcramer <gcramer@3f837a8a-01ec-4976-b98b-b3b4b9fec2bf>
2012-11-05 17:03:15 +0000

    minor fix

--- trunk/tcl/app-analysis.tcl
+++ trunk/tcl/app-analysis.tcl
@@ -524,7 +524,7 @@ proc SetState {state} {
 	variable Vars
 
 	after cancel $Vars(after)
-	if {![winfo exists $Vars(toolbar:childs)]} { return }
+	if {![winfo exists $Vars(tree)]} { return }
 	if {$Vars(state) eq $state} { return }
 	set Vars(state) $state
 
@@ -789,7 +789,6 @@ proc Signal {id code} {
 	variable Vars
 
 	set parent [winfo toplevel $Vars(tree)]
-	SetState disabled
 
 	if {[string is integer $code]} {
 		set msg [format $mc::Signal(terminated) $code]
@@ -807,6 +806,7 @@ proc Signal {id code} {
 		}
 	}
 
+	SetState disabled
 	set Vars(engine:id) -1
 }
 
