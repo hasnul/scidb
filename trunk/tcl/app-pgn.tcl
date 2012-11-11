1272c81f00eecd5fd407383a8f7c7612a9207f28
gcramer <gcramer@3f837a8a-01ec-4976-b98b-b3b4b9fec2bf>
2012-11-11 16:40:46 +0000

    problem in game editor fixed

--- trunk/tcl/app-pgn.tcl
+++ trunk/tcl/app-pgn.tcl
@@ -1144,7 +1144,17 @@ proc InsertMove {position w level key data} {
 					")" { $w insert current " )" bracket }
 					"e" { $w insert current "\n"; $w insert current "<$mc::EmptyGame>" empty }
 					"s" { if {[$w index current] ne "1.1"} { $w insert current "\n" } }
-					"]" { if {$flag && $level > $Options(indent:max)} { $w insert current "\]" bracket } }
+
+					"]" {
+						if {$flag && $level > $Options(indent:max)} {
+							set txt "]"
+						} else {
+							# NOTE: We need a blind character between the marks because
+							# the editor is permuting consecutive marks.
+							set txt "\u200b"
+						}
+						$w insert current $txt bracket
+					}
 
 					default {
 						variable cursor::collapse
