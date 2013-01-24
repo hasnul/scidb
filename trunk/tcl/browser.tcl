8c0df44059a0dbc5768df72b2ebea8ff5b44b973
gcramer <gcramer@3f837a8a-01ec-4976-b98b-b3b4b9fec2bf>
2013-01-24 23:07:55 +0000

    some fixes

--- trunk/tcl/browser.tcl
+++ trunk/tcl/browser.tcl
@@ -725,13 +725,6 @@ proc Goto {position step} {
 
 	::scidb::game::go $position $step
 
-	lassign [::scidb::pos::inHand? $position] matw matb
-
-	if {[info exists Vars(holding:w)]} {
-		::board::holding::update $Vars(holding:w) $matw
-		::board::holding::update $Vars(holding:b) $matb
-	}
-
 	if {$Vars(autoplay)} {
 		if {[::scidb::game::position $position atEnd?]} {
 			ToggleAutoPlay $position
@@ -1048,6 +1041,11 @@ proc UpdatePGN {position data {w {}}} {
 					set Vars(current) $key
 					if {[llength $previous] && $Vars(active) eq $previous} {
 						EnterMove $w $position $previous
+					}
+					if {[info exists Vars(holding:w)]} {
+						lassign [::scidb::pos::inHand? $position] matw matb
+						::board::holding::update $Vars(holding:w) $matw
+						::board::holding::update $Vars(holding:b) $matb
 					}
 				}
 			}
