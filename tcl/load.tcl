d6f1bfe0e1cd7b72afa099cf7cf9b019422172c4
gcramer <gcramer@3f837a8a-01ec-4976-b98b-b3b4b9fec2bf>
2013-03-31 00:13:33 +0000

    fixes for 3rd preview version

--- tcl/load.tcl
+++ tcl/load.tcl
@@ -67,7 +67,7 @@ proc source {path args} {
 	variable Log
 	variable currentFile
 
-	array set opts { -message "" -encoding "" }
+	array set opts { -message "" -encoding "" -throw 0 }
 	array set opts $args
 	set msg $opts(-message)
 
@@ -82,7 +82,9 @@ proc source {path args} {
 	set enc {}
 	if {[string length $opts(-encoding)]} { set enc [list -encoding $opts(-encoding)] }
 
-	if {[catch {uplevel ::source {*}$enc [list $path]} err]} {
+	if {$opts(-throw)} {
+		uplevel ::source {*}$enc [list $path]
+	} elseif {[catch {uplevel ::source {*}$enc [list $path]} err]} {
 		lappend Log error [format $mc::FileIsCorrupt $path] error $err
 	} elseif {[string length $msg]} {
 		lappend Log info "$msg: $path"
