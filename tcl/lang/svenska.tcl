c02ce26f1ac05e620e628c5b4289f3843f6b7b46
gcramer <gcramer@3f837a8a-01ec-4976-b98b-b3b4b9fec2bf>
2013-05-26 14:19:02 +0000

    change accelarator for "edit comment" from Ctrl-C to Ctrl-K, because Ctrl-C is now bound to "copy to primary selection", and this is the default binding

--- tcl/lang/svenska.tcl
+++ tcl/lang/svenska.tcl
@@ -402,7 +402,7 @@
 ::application::board::mc::SwitchView(list)			"Switch to game list view" ;# NEW
 
 ::application::board::mc::Accel(edit-annotation)	"A"
-::application::board::mc::Accel(edit-comment)		"C"
+::application::board::mc::Accel(edit-comment)		"K"
 ::application::board::mc::Accel(edit-marks)			"M"
 ::application::board::mc::Accel(add-new-game)		"S" ;# NEW
 ::application::board::mc::Accel(replace-game)		"R" ;# NEW
