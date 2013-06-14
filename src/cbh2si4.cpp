fd6c88444bff34f4cd8152dccd38a1ab73f4397c
gcramer <gcramer@3f837a8a-01ec-4976-b98b-b3b4b9fec2bf>
2013-06-14 17:18:52 +0000

    compiler warnings fixed

--- src/cbh2si4.cpp
+++ src/cbh2si4.cpp
@@ -396,6 +396,7 @@ logIOError(IOException const& exc, unsigned gameNumber = 0)
 		case IOException::Write_Failed:
 		case IOException::Encoding_Failed:
 		case IOException::Max_File_Size_Exceeded:
+		case IOException::Not_Original_Version:
 			return; // cannot happen
 	}
 
@@ -406,6 +407,7 @@ logIOError(IOException const& exc, unsigned gameNumber = 0)
 		case IOException::Game:				file = "game"; break;
 		case IOException::Namebase:		file = "namebase"; break;
 		case IOException::Annotation:		file = "annotation"; break;
+		case IOException::PgnFile:			/* cannot happen */ break;
 	}
 
 	mstl::string msg("Error");
