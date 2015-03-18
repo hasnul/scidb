3cb0fdb5fffb3dd8eab78f6cf2aa3618219ce1c0
gcramer <gcramer@3f837a8a-01ec-4976-b98b-b3b4b9fec2bf>
2015-03-18 18:19:33 +0000

    revert some changes

--- trunk/tcl/crosstable.tcl
+++ trunk/tcl/crosstable.tcl
@@ -915,7 +915,7 @@ proc Destroy {dlg w unsubscribe} {
 }
 
 
-proc Open {dlg which gameIndex {stimulate 0}} {
+proc Open {dlg which gameIndex} {
 	variable ${dlg}::Vars
 
 	Tooltip $dlg hide
@@ -933,10 +933,6 @@ proc Open {dlg which gameIndex {stimulate 0}} {
 		set info [::scidb::db::get gameInfo $index $viewId $base $variant]
 		set Vars(${which}Id) [::widget::busyOperation \
 			[list ::${which}::load $path $base $variant $info $viewId $index $Vars(${which}Id)]]
-	}
-
-	if {$stimulate} {
-		after idle [list $Vars(html) stimulate]
 	}
 }
 
@@ -1103,8 +1099,8 @@ proc Mouse1Down {dlg nodes} {
 	foreach node $nodes {
 		set gameIndex [$node attribute -default {} game]
 		if {[string length $gameIndex]} {
-			after idle [namespace code [list Open $dlg browser $gameIndex 1]]
 			::tooltip::hide
+			Open $dlg browser $gameIndex
 		}
 
 		# don't use because Button-1 is used for highlighting
