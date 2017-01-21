b338fe67d34d93dcee5d487579544905c38931d9
gcramer <gcramer@3f837a8a-01ec-4976-b98b-b3b4b9fec2bf>
2017-01-21 11:58:07 +0000

    fix in drag&drop

--- tcl/dnd/tkdnd_unix.tcl
+++ tcl/dnd/tkdnd_unix.tcl
@@ -144,7 +144,7 @@ proc xdnd::_HandleXdndPosition { drop_target rootX rootY drag_source } {
       }
     }
   }
-  
+ 
   # debug "\t($_drop_target) -> ($drop_target)"
   if {$drop_target != $_drop_target} {
     if {[string length $_drop_target]} {
@@ -343,7 +343,7 @@ proc xdnd::_HandleXdndDrop { time } {
 };# xdnd::_HandleXdndDrop
 
 # ----------------------------------------------------------------------------
-#  Command xdnd::_HandleXdndDrop
+#  Command xdnd::_HandleDragPosition
 # ----------------------------------------------------------------------------
 proc xdnd::_HandleDragPosition { window rootX rootY } {
   variable _types
