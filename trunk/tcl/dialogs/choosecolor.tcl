af265b171f85fe49274be4a579751ee8ddcd3224
gcramer <gcramer@3f837a8a-01ec-4976-b98b-b3b4b9fec2bf>
2017-07-08 15:51:09 +0000

    Reverting changes.

--- trunk/tcl/dialogs/choosecolor.tcl
+++ trunk/tcl/dialogs/choosecolor.tcl
@@ -1633,8 +1633,6 @@ proc MakeFrame {container} {
 
 	set height	[expr {$Priv(notebook-size:y) - 6}]
 	set width	[expr {$Priv(notebook-size:x) - 6}]
-incr width 50 ;# XXX
-puts "---> MakeFrame: $height $width"
 
 	set canvas [tk::canvas $container.list \
 		-yscrollcommand [ list $container.vsb set] \
